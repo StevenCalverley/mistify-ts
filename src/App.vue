4fc92a67806fa76106d274ad8e06e2a04b8c818d
Steve Calverley <steve.calverley@heffron.com.au>
2021-01-01 22:08:04 +1100

    Install Tailwind - add tailwind 2.02 - add button component - remove boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <header class="p-4 bg-gray-800">
+      <h1 class="text-3xl text-gray-50">Mistify</h1>
+    </header>
+    <main class="mt-4 container mx-auto">
+      <m-button id="test">Test</m-button>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import MButton from "./components/MButton";
 
 export default Vue.extend({
   name: "App",
   components: {
-    HelloWorld
+    MButton
   }
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/MButton.ts
@@ -0,0 +1,78 @@
+import Vue, { CreateElement, VNode } from "vue";
+
+const MButton = Vue.extend({
+  name: "MButton",
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    tag: {
+      type: String,
+      default: "button",
+      validator(value) {
+        return ["button", "a"].indexOf(value) !== -1;
+      }
+    },
+    value: {
+      type: [String, Number],
+      default: null
+    },
+    classes: {
+      type: [String, Array, Object],
+      default:
+        "block px-4 py-2 text-white transition duration-100 ease-in-out bg-blue-500 border border-transparent rounded shadow-sm hover:bg-blue-600 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50  disabled:opacity-50 disabled:cursor-not-allowed"
+    }
+  },
+  render(createElement) {
+    const renderFun: (createElement: CreateElement) => VNode = this.render;
+    return renderFun(createElement);
+  },
+  methods: {
+    blurHandler(e: FocusEvent) {
+      this.$emit("blur", e);
+    },
+    focusHandler(e: FocusEvent) {
+      this.$emit("focus", e);
+    },
+    clickHandler(e: MouseEvent) {
+      this.$emit("click", e);
+    },
+    keydownHandler(e: MouseEvent) {
+      this.$emit("keydown", e);
+    },
+    mousedownHandler(e: MouseEvent) {
+      this.$emit("mousedown", e);
+    },
+    /**
+     * Attrs according to the button type
+     * @return {Object}
+     */
+    getAttributes() {
+      return {
+        id: this.id,
+        value: this.value,
+        type: "button"
+      };
+    },
+    render(createElement: CreateElement): VNode {
+      return createElement(
+        this.tag,
+        {
+          attrs: this.getAttributes(),
+          class: this.classes,
+          on: {
+            click: this.clickHandler,
+            focus: this.focusHandler,
+            blur: this.blurHandler,
+            keydown: this.keydownHandler,
+            mousedown: this.mousedownHandler
+          }
+        },
+        this.$slots.default
+      );
+    }
+  }
+});
+
+export default MButton;
