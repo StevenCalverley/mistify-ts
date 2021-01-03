caf6bb9b56a973e9fe4226d890f972016da623ae
Steve Calverley <steve.calverley@heffron.com.au>
2021-01-04 06:10:23 +1100

    MBadge - Fix type

--- src/components/MBadge/MBadge.vue
+++ src/components/MBadge/MBadge.vue
@@ -2,7 +2,7 @@
   <span
     class="inline-block px-2 py-1 text-sm font-bold rounded-full"
     :class="[
-      { ' bg-gray-100 text-gray-800': type === '' || type === ' GRAY' },
+      { ' bg-gray-100 text-gray-800': type === '' || type === 'GRAY' },
       { ' bg-blue-100 text-blue-800': type === 'BLUE' },
       { ' bg-yellow-100 text-yellow-800': type === 'YELLOW' },
       { ' bg-green-100 text-green-800': type === 'GREEN' },
